955fa9361765bacc4b3318608090f74f52018367
James <jamesandrewpike@gmail.com>
2024-04-20 18:00:53 -0400

    Add Menu Header

--- components/tabs2.tsx
+++ components/tabs2.tsx
@@ -39,19 +39,28 @@ import {AcmeIcon} from "./social";
 
 import NotificationsCard from "./notifications-card";
 import ThemeDropdown from "./theme-dropdown";
+import Antipasti from "./antipasti";
 import Primtab from "./primtab";
 
 export default function Tab2(props: CardProps) {
   return (
     <Card {...props}>
-            <div  style={{ overflowX: 'auto' }}> {/* Container for tabs */}
+        <div  style={{ overflowX: 'auto' }}> {/* Container for tabs */}
             <div className="w-full max-w-[1024px] px-4 lg:px-8"></div>
-            <header className="mb-6 flex w-full items-center justify-between mt-6 px-4">
+            <header className="mb-6 flex w-full items-center justify-between mt-6 px-6">
             <div className="flex flex-col">
-              <h1 className="text-xl font-bold text-default-900 lg:text-3xl">Menu</h1>
-              <p className="text-small text-default-400 lg:text-medium">Wellington Menu</p>
+              <h1 className="text-2xl font-bold text-default-900 lg:text-3xl">Menu</h1>
+              <p className="text-medium text-default-400 lg:text-medium">Wellington Menu</p>
             </div>
-         <ThemeDropdown/>
+            <Button
+              className="bg-foreground text-background"
+              startContent={
+                <Icon className="flex-none text-background/60" icon="lucide:home" width={16}/>
+              }
+            >
+              Wellington
+              <Icon className="flex-none text-background/60" icon="lucide:arrow-down" width={16}/>
+            </Button>
           </header>
 <div>
                 {/* <div className="flex items-center justify-between px-5 pt-6">
