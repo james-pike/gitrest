2ef3991f98b0092fa9cb6556cda4a093b8737924
James <jamesandrewpike@gmail.com>
2024-04-24 16:36:04 -0400

    Add drawer sidebar

--- components/socials.tsx
+++ components/socials.tsx
@@ -7,6 +7,8 @@ import {Button, Input, Link} from "@nextui-org/react";
 import {Icon} from "@iconify/react";
 
 import {AcmeIcon} from "../social";
+import ThemeSwitch from "@/theme-switch";
+import { ThemeButton } from "./theme-button";
 
 
 type SocialIconProps = Omit<IconProps, "icon">;
@@ -42,14 +44,19 @@ export default function Socials() {
   return (
             <div className="">
                 <div className="h-8"></div>
-            <div className="flex space-x-6 pt-10 px-1 pb-4 border-t-2 border-default">
-              {footerNavigation.social.map((item) => (
-                <Link key={item.name} isExternal className="text-default-400" href={item.href}>
-                  <span className="sr-only">{item.name}</span>
-                  <item.icon aria-hidden="true" className="w-6 text-xl" />
-                </Link>
-              ))}
-            </div>
+                <div className="flex justify-between space-x-6 pt-10 px-1 pb-4 border-t-2 border-default">
+  {footerNavigation.social.map((item) => (
+    <Link key={item.name} isExternal className="text-default-400" href={item.href}>
+      <span className="sr-only">{item.name}</span>
+      <item.icon aria-hidden="true" className="w-6 text-xl" />
+    </Link>
+  ))}
+  {/* Add the icon you want to align to the right */}
+  <div className="flex items-center">
+    {/* Your icon component */}
+    <ThemeButton />
+  </div>
+</div>
             </div>
     
   );
