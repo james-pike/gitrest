a6a185b872f6af46af3662af68da4363d04435ef
James <jamesandrewpike@gmail.com>
2024-04-25 18:40:28 -0400

    commit

--- components/nav2.tsx
+++ components/nav2.tsx
@@ -146,7 +146,7 @@ export default function Nav2(props: NavbarProps) {
                 as={Link}
                 href="/#"
                 variant="faded"
-                className="flex-1 mr-2 hover:bg-green-700 hover:border-white bg-default-400 border-green-800 border-1.5"
+                className="flex-1 mr-2 rounded-lg hover:bg-green-700 hover:border-white bg-default-400 border-green-800 border-1.5"
                 onClick={() =>
                   window.open(
                     "https://www.ubereats.com/ca/store/joes-italian-kitchen/Z4rz0qIwTSq1vaWJQSrLRw "
@@ -159,7 +159,7 @@ export default function Nav2(props: NavbarProps) {
                 as={Link}
                 href="/"
                 variant="faded"
-                className="flex-1 hover:border-white bg-default-200 border-1.5 border-default-500"
+                className="flex-1 hover:border-white rounded-lg bg-default-200 border-1.5 border-default-500"
                 onClick={() =>
                   window.open(
                     "https://joesitaliankitchen-1asc.mobi2go.com"
@@ -172,7 +172,7 @@ export default function Nav2(props: NavbarProps) {
                 as={Link}
                 href=""
                 variant="faded"
-                className="flex-1 ml-2 hover:border-white bg-default-50 border-white border-1.5"
+                className="flex-1 ml-2 hover:border-white rounded-lg bg-default-50 border-white border-1.5"
                 onClick={onOpen}
               >
                 RESERVATIONS
