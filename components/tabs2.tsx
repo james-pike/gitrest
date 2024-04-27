5ffbdd18c5da8623e15cb4500bf4463c35622f4e
James <jamesandrewpike@gmail.com>
2024-04-27 17:47:35 -0400

    commit

--- components/tabs2.tsx
+++ components/tabs2.tsx
@@ -29,10 +29,11 @@ export default function Tab2({ initialLocation = "Wellington", ...props }: { ini
 
   return (
     <Card {...props}>
-      <div style={{ overflowX: 'auto' }}>
-        <header className="mb-3 flex w-full items-center justify-between mt-5 pl-5 pr-3">
-          <div className="flex flex-col">
-            <h1 className="text-2xl font-bold text-default-900 lg:text-3xl">Menu</h1>
+      <div className="md:mx-auto" style={{ overflowX: 'auto' }}>
+        <img src="/images/a1.jpg" alt="menu" className="w-full h-40 object-cover" />
+        <header className="mb-3 flex w-full items-center  justify-between mt-5 pl-5 pr-3">
+          <div className="flex flex-col ">
+            <h1 className="text-2xl font-bold text-default-900  lg:text-3xl">Menu</h1>
             <p className="text-medium text-default-400 lg:text-medium">{selectedLocation} {selectedLocation === "Nepean" ? "Express" : ""} Menu</p>
           </div>
           <Dropdown>
