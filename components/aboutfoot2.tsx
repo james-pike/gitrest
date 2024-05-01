1dbbfe29d7594afc89f73af0be494981f55c5752
James Pike <jamesandrewpike@gmail.com>
2024-04-30 21:20:58 -0400

    commit

--- components/aboutfoot2.tsx
+++ components/aboutfoot2.tsx
@@ -176,7 +176,7 @@ export default function AboutFooter2() {
                     </div>
 
 
-                    <div className="border-4 border-black"></div>
+                    <div className="border-8 border-black"></div>
 
                     <div style={{ position: 'relative', height: '30vh' }}>
 <Image
