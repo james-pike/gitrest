6ce2a3cf0a7d2974359e807179ea2b7417ae1908
James Pike <jamesandrewpike@gmail.com>
2024-04-30 21:08:25 -0400

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,6 @@
-'use client'
-
-import AboutFooter from "@/components/aboutfooter";
-import BasicFooter from "@/components/footer";
-import { useEffect, useState } from "react";
+import AboutFooter2 from "@/components/aboutfoot2";
 
 export default function HomePage() {
-	const [isMobile, setIsMobile] = useState(false);
-
-	useEffect(() => {
-		const handleResize = () => {
-			setIsMobile(window.innerWidth <= 568); // Adjust the threshold as needed
-		};
-
-		handleResize(); // Check initial width
-		window.addEventListener("resize", handleResize);
-
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
 
 	return (
 		<AboutFooter2 />
