d490b11df230025ed534ca61646d0aa209dbd03f
James <jamesandrewpike@gmail.com>
2024-06-04 17:34:17 -0400

    commit

--- components/video.tsx
+++ components/video.tsx
@@ -1,21 +1,22 @@
         //video.tsx
 
         import React from 'react';
+        import Image from "next/image";
 
         export default function Video() {
             
             return (
                 <div className="relative ">
-                    <video
-                        src={"/video/almonte1.mp4"}
-                        loop
-                        autoPlay
-                        muted
-                        style={{ opacity: 0.75, height: '40vh' }}
-                        className='object-cover w-full h-full lg:px-40 '
-                        poster='/images/a6.png'
-                    >
-                    </video>
+              <Image
+                            className=" object-cover w-full "
+                            src="/images/g1.jpg"
+                            alt="{name}"
+                            quality={50}
+                            width={440}
+                            height={0}
+                            style={{ width: 'full', height: '16em' }} // optional
+                            priority
+                        />
                     <div className="absolute top-1/2 left-1/4 transform -translate-x-1/4 -translate-y-1/2 z-20 space-y-3">
                         <h1 className="text-6xl text-white font-bold">JOES ITALIAN KITCHEN</h1>
                         <p className="text-2xl text-white">Lorem Ipsum serving Almonte, Preston, & Wellington</p>
