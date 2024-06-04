155b10a2b2da067a4aac0671821677f2304685d6
James <jamesandrewpike@gmail.com>
2024-06-04 17:36:49 -0400

    commit

--- components/video.tsx
+++ components/video.tsx
@@ -9,12 +9,12 @@
                 <div className="relative ">
               <Image
                             className=" object-cover w-full "
-                            src="/images/g1.jpg"
+                            src="/images/a6.png"
                             alt="{name}"
                             quality={50}
                             width={440}
                             height={0}
-                            style={{ width: 'full', height: '16em' }} // optional
+                            style={{ width: 'full', height: '30em' }} // optional
                             priority
                         />
                     <div className="absolute top-1/2 left-1/4 transform -translate-x-1/4 -translate-y-1/2 z-20 space-y-3">
