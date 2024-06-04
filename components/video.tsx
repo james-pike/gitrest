f6a0c2ed71455bf3c2a761081daa16d71c0e171b
James <jamesandrewpike@gmail.com>
2024-06-04 17:06:46 -0400

    commit

--- components/video.tsx
+++ components/video.tsx
@@ -17,7 +17,7 @@
                     >
                     </video>
                     <div className="absolute top-1/2 left-1/4 transform -translate-x-1/4 -translate-y-1/2 z-20 space-y-3">
-                        <h1 className="text-6xl text-white font-bold">Menu</h1>
+                        <h1 className="text-6xl text-white font-bold">JOES ITALIAN KITCHEN</h1>
                         <p className="text-2xl text-white">This content should display the Wellington Menu</p>
                     </div>
                 </div>
