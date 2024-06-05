7582a7fd1ce03e50e475f44720b4f24a256d1f69
James <jamesandrewpike@gmail.com>
2024-06-04 22:18:32 -0400

    commit

--- components/video.tsx
+++ components/video.tsx
@@ -20,8 +20,8 @@
                     <div className="absolute top-1/2 left-1/4 transform -translate-x-1/4 -translate-y-1/2 z-20 space-y-3">
                         <h1 className="text-6xl text-white font-bold">JOES ITALIAN KITCHEN</h1>
                         <p className="text-2xl text-white">Lorem Ipsum serving Almonte, Preston, & Wellington</p>
-                        <button className="bg-white text-black font-bold py-4 border-white border-1 px-4 rounded mr-2 text-medium">ORDER ONLINE</button>
-                        <button className="bg-white text-black font-bold py-4 border-white border-1 px-4 rounded mr-2 text-medium">RESERVATIONS</button>
+                        <button className="bg-white text-black font-bold py-3 border-white border-1 px-4 rounded mr-2 text-large">ORDERS</button>
+                        <button className="bg-white text-black font-bold py-3 border-white border-1 px-4 rounded mr-2 text-large">RESERVATIONS</button>
                     </div>
                 </div>
             );
